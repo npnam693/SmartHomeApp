48e4db2007f31cfe83122c5de4afed6c7462fa5f
TruongNguyen1305 <trngcr71305@gmail.com>
2023-05-04 21:59:46 +0700

    test

--- api/axiosSetup.js
+++ api/axiosSetup.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 export const axiosClient = axios.create({
     // baseURL: 'https://api-smarthome.onrender.com/'
-    // baseURL: 'http://10.0.2.2:3000/'
-    baseURL: "https://smarthome-api.onrender.com"
+    baseURL: 'http://10.0.2.2:3000/'
+    //baseURL: "https://smarthome-api.onrender.com"
 });
 
 export const axiosAdafruit =axios.create({
